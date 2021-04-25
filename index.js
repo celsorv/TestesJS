4c3ed9e26d1010a3468bc28e261207b91f86ab94
Celso Vitorino <>
2021-04-25 14:41:11 +0000

    comentários

--- index.js
+++ index.js
@@ -4,13 +4,18 @@
 
 (() => {
 
+  /* na arrow function, this sempre faz   referência ao escopo em que ele foi criado.
+  */
   this.name = 'arrow function';
+  
   const getNameArrowFn = () => this.name;
 
   function getName() {
     return this.name;
   }
 
+  /* na função formato tradicional, this faz referência ao escopo de onde a função está sendo chamada.
+  */
   const user = {
     name: 'Nome no objeto de execução',
     getNameArrowFn,
